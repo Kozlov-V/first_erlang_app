b11666e376d594fea54f314289c82a2f5edaeaf0
Mirosław Pragłowski <m@praglowski.com>
2015-08-10 01:37:37 +0200

    Add start function (needed to run application via erl -s)

--- src/firstapp_app.erl
+++ src/firstapp_app.erl
@@ -8,12 +8,19 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2
+-export([start/0
+        ,start/2
         ,stop/1]).
 
 %%====================================================================
 %% API
 %%====================================================================
+
+start() ->
+  ok = application:start(crypto),
+  ok = application:start(ranch),
+  ok = application:start(cowboy),
+  ok = application:start(firstapp).
 
 start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([
@@ -24,7 +31,6 @@ start(_StartType, _StartArgs) ->
     ),
     'firstapp_sup':start_link().
 
-%%--------------------------------------------------------------------
 stop(_State) ->
     ok.
 
