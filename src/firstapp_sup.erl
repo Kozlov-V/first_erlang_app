2c9b3373f8d190deb9ca1ceb3b55751e8bae453d
Mirosław Pragłowski <m@praglowski.com>
2015-08-15 20:14:19 +0200

    CHILD macro from https://github.com/robashton/vir/blob/master/templates/web/apps/%24app/src/%24app_sup.erl

--- src/firstapp_sup.erl
+++ src/firstapp_sup.erl
@@ -15,6 +15,9 @@
 
 -define(SERVER, ?MODULE).
 
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
 %%====================================================================
 %% API functions
 %%====================================================================
